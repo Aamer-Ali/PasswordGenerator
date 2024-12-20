61456221f106536d777921694d28a51ba31ab661
Sayyed Aamer Ali <sayyed.aamer.ali@gmail.com>
2024-12-20 15:04:11 +0530

    Update App.tsx
    Create password logic is created.
    Generated random number and use that number as index to get the character from the characters string to generate the password. this goes in the loop for password length time.

--- App.tsx
+++ App.tsx
@@ -20,7 +20,16 @@ const App = () => {
   const [symbols, setSymbols] = useState(false);
 
   const generatePasswordString = (passwordLength: number) => {};
-  const createPassword = (characters: string, passwordLength: number) => {};
+
+  const createPassword = (characters: string, passwordLength: number) => {
+    let result = '';
+    for (let i = 0; i < passwordLength; i++) {
+      const characterIndex = Math.round(Math.random() * characters.length);
+      result += characters.charAt(characterIndex);
+    }
+    return result;
+  };
+
   const resetPasswordState = () => {};
 
   return (
