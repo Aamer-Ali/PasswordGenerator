c7df55b607d75ff2b51ca866f5cef64663c66d2c
Sayyed Aamer Ali <sayyed.aamer.ali@gmail.com>
2024-12-20 09:41:18 +0530

    Yup added
    Yup package added for validation.
    Password validation schema is created.
    Also lock file added to git ignore.

--- App.tsx
+++ App.tsx
@@ -1,6 +1,16 @@
 import {Text, View} from 'react-native';
 import React from 'react';
 
+//Form validation imports
+import * as Yup from 'yup';
+
+const PasswordSchema = Yup.object().shape({
+  passwordValidation: Yup.number()
+    .min(4, 'Length must be more that 4 characters')
+    .max(16, 'Length must be less that 16 characters')
+    .required('The length is required'),
+});
+
 const App = () => {
   return (
     <View>
